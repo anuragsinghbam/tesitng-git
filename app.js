ccb8bde85a40fe9f95af287e0f12c1c0b1aa3c78
Anurag Singh <anuragsinghbam@gmail.com>
2021-02-14 10:27:23 +0530

    Changed background color from cyan to purple

---
+++ app.js
@@ -0,0 +1,7 @@
+const home = document.getElementById("homepage")
+var subtitle = document.createElement("h6")
+
+
+subtitle.innerText = "Subtitle injected by JavaScript"
+
+home.appendChild(subtitle)
